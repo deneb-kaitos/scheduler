5da8892d2f2c627befd55a5f2735ca744eab3c66
Dmitry Medvedev <dmitry.medvedev@gmail.com>
2023-05-23 22:12:23 +0200

    global.css moved to static

---
+++ sources/front-end/src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script>
+	import { env } from '$env/dynamic/public';
+	import { browser as IsInBrowser } from '$app/environment';
+	import { onMount, onDestroy } from 'svelte';
+	import Header from '$lib/containers/Header/index.svelte';
+	import Footer from '$lib/containers/Footer/index.svelte';
+	import 'inter-ui/inter.css';
+
+	// import {
+	//   WsStoreAdapter,
+	// } from '$lib/modules/wsStoreAdapter/index.mjs';
+	// import {
+	//   Ldr,
+	// } from '$lib/modules/ldr/ldr.mjs';
+
+	// /** @type {Ldr | null} */
+	// let ldr = null;
+	// /** @type {WsStoreAdapter | undefined | null} */
+	// let wsStoreAdapter = null;
+
+	const workersConfig = {
+		wsWorker: {
+			url: `${env.PUBLIC_WS_PROTO}://${env.PUBLIC_WS_HOST}:${env.PUBLIC_WS_PORT}/`
+		}
+	};
+
+	const handleUIChannelMessages = (/** @type {MessageEvent} */ uiMessage) => {
+		const { data } = uiMessage;
+		console.log('handleUIChannelMessages', data);
+	};
+
+	onMount(() => {
+		if (IsInBrowser === true) {
+			// uiBroadcastChannel.addEventListener('message', handleUIChannelMessages);
+			// isMobileDevice = IsInDevMode === true ? true : window.matchMedia("(any-pointer: none)").matches;
+			// @ts-ignore
+			// ldr = new Ldr(workersConfig);
+			// await ldr?.start();
+			// wsStoreAdapter = new WsStoreAdapter();
+			// wsStoreAdapter.start();
+		}
+	});
+
+	onDestroy(() => {
+		if (IsInBrowser === true) {
+			// await ldr?.stop();
+			// ldr = null;
+			// if (wsStoreAdapter) {
+			//   wsStoreAdapter.stop();
+			//   wsStoreAdapter = null;
+			// }
+		}
+	});
+</script>
+
+<Header />
+<main>
+	<slot />
+</main>
+<Footer />
--- sources/front-end/src/routes/+page.svelte
+++ sources/front-end/src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { browser as IsInBrowser } from '$app/environment';
+	import { onMount, onDestroy } from 'svelte';
+
+	let isAuthenticated = false;
+	let items = [];
+
+	onMount(() => {
+		if (IsInBrowser === true) {
+			items.push({
+				id: window.crypto.randomUUID(),
+				title: 'samowar',
+				isVirtual: false
+			});
+			items.push({
+				id: window.crypto.randomUUID(),
+				title: 'lemke',
+				isVirtual: false
+			});
+			items.push({
+				id: window.crypto.randomUUID(),
+				title: 'muha',
+				isVirtual: false
+			});
+
+			items = items;
+		}
+	});
+
+	onDestroy(() => {});
+</script>
+
+<article>
+	{#if isAuthenticated === true}
+		{#each items as item (item.id)}{/each}
+	{:else}
+		not isAuthenticated
+	{/if}
+</article>
+
+<style>
+	article {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 1fr 1fr 1fr;
+		gap: var(--main-grid-gap);
+		height: 100%;
+	}
+</style>
+
